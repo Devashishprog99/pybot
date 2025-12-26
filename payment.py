f9193bbcc95a4e587c5a4dd0e0b11f2da8d9fd48
batmanisaliveebro-cloud <devashishhswami@gmail.com>
2025-12-26 07:12:23 +0530

    Clean up logs by suppressing SSL warnings and adding certifi

--- payment.py
+++ payment.py
@@ -4,6 +4,9 @@ Cashfree Payment Gateway Integration
 import asyncio
 import qrcode
 import os
+import urllib3
+# Suppress InsecureRequestWarning from Cashfree SDK
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 from io import BytesIO
 from datetime import datetime, timedelta
 from cashfree_pg.models.create_order_request import CreateOrderRequest
